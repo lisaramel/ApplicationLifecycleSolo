dd6c19a7903bc83ac701d33fe3b026315aaf3e9a
lisaramel <lisa.ramel@yh.nackademin.se>
2021-05-14 09:40:38 +0200

    added test methods

--- src/test/java/com/example/springdocker/MyMathCalcTest.java
+++ src/test/java/com/example/springdocker/MyMathCalcTest.java
@@ -13,15 +13,34 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MyMathCalcTest {
 
+    MyMathCalc calc = new MyMathCalc();
+
     @Test
     void add() {
+        int expected = 5;
+        int actual  = calc.add(2, 3);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void multiply() {
+        int expected = 6;
+        int actual  = calc.multiply(2, 3);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void divide() {
+        double expected = 2;
+        double actual  = calc.divide(4, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void divideThrows() {
+        assertThrows(IndexOutOfBoundsException.class, () -> calc.divide(2, 0));
     }
 }

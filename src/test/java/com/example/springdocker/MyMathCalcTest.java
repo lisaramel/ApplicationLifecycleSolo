50cb34f9f5fc8930e54ab23cc061fca952e05bb7
lisaramel <lisa.ramel@yh.nackademin.se>
2021-05-14 09:28:05 +0200

    added tests for MyMathCalc

---
+++ src/test/java/com/example/springdocker/MyMathCalcTest.java
@@ -0,0 +1,27 @@
+package com.example.springdocker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Lisa Ramel
+ * Date: 2021-05-14
+ * Time: 09:24
+ * Project: springdocker
+ * Copywrite: MIT
+ */
+class MyMathCalcTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
